4ee08dc85ed9d29f7171d41d2b22e01976e60aab
williansbriones <107446673+williansbriones@users.noreply.github.com>
2023-09-01 18:41:46 +0000

    login terminado

--- miapp/src/app/home/home.page.ts
+++ miapp/src/app/home/home.page.ts
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  email: string = "";
+  contrasena: string = "";
+
   constructor() {}
+
+  getconsultas(){
+    let errorContraseña = document.getElementsByClassName("contraseña-incorrecta") as HTMLCollectionOf<HTMLElement>;
+    var i = 0;
+    errorContraseña[0].style.display = "block"
+    console.log(this.email);
+    console.log(this.contrasena);
+    return
+  }
 
 }
