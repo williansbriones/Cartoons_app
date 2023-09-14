f9278a288e3497afcbd31552a17ae6dfe854ddbe
williansbriones <107446673+williansbriones@users.noreply.github.com>
2023-09-14 16:12:31 +0000

    ingreso de alerta cerrar sesion

--- miapp/src/app/principal/principal.page.ts
+++ miapp/src/app/principal/principal.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.page.html',
@@ -7,8 +8,41 @@ import { NavController } from '@ionic/angular';
 })
 export class PrincipalPage implements OnInit {
 
-  constructor(private navctrl: NavController) { }
+  constructor(
+    private navctrl: NavController,
+    private alertController: AlertController
+    ) { }
 
+  nombre: string = "PepeGrillo";
+
+  async alertCerraSesion() {
+    const alert = await this.alertController.create({
+      header: '¿Seguro que quieres cerrar sesión?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          
+          handler: () => {
+            
+          },
+        },
+        {
+          text: 'cerrra sesión',
+          role: 'confirm',
+          handler: () => {
+            this.CerrarSesion()
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    
+  }
+  
 
   CerrarSesion(){
     this.navctrl.navigateRoot("home")
