5acc49213b061cc60fa2f608b950cf8c2de6e363
williansbriones <107446673+williansbriones@users.noreply.github.com>
2023-09-15 21:55:46 +0000

    Ultimo ingreso antes de presentacion

--- miapp/src/app/docente/docente.page.ts
+++ miapp/src/app/docente/docente.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-docente',
@@ -7,7 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocentePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private navctrl: NavController,
+    private alertController: AlertController
+  ) { }
+  
+  nombre: string = "Danilo Maldonado";
+
+  async alertCerraSesion() {
+    const alert = await this.alertController.create({
+      header: '¿Seguro que quieres cerrar sesión?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          
+          handler: () => {
+            
+          },
+        },
+        {
+          text: 'cerrra sesión',
+          role: 'confirm',
+          handler: () => {
+            this.CerrarSesion()
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    
+  }
+  
+
+  CerrarSesion(){
+    this.navctrl.navigateRoot("home")
+  }
+
+
 
   ngOnInit() {
   }
